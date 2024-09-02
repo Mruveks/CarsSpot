501b81d5c2d7aa56a5fec3271fa518c41e5e6d24
Mruveks <Mruvekss@gmail.com>
2024-09-02 21:49:43 +0200

    Update footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 flex w-screen bg-bg-secondary text-white border-white py-6 px-[120px] border-t-1 justify-between">
+      <div className="h-6">Cars Spot</div>
+      <div className="h-6 underline">Polityka prywatności</div>
+    </footer>
+  );
+};
+
+export default Footer;
